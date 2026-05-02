4750321d3fdfb95e6d532719b5375dc2454048b2
bosco227 <antoniobosco2612@gmail.com>
2026-05-02 16:21:03 -0300

    Feita a build do site(Teste)'

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:3000/agendamentos";
-
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:3000/agendamentos";
 // BUSCAR TODOS (GET)
 export async function buscarAgendamentos() {
   const res = await fetch(BASE_URL);
